a1b07a0804bcf3f265acc72bb396eb78aecc1f5d
Pratik Kuril <pratikkuril16@gmail.com>
2025-04-22 19:53:35 +0530

    Add groq queries

---
+++ src/sanity/lib/queries.ts
@@ -0,0 +1,13 @@
+import { defineQuery } from "next-sanity";
+
+export const POSTS_QUERY = defineQuery(`
+    *[_type == "post" && defined(slug.current)]
+    [0...12]
+    {_id, title, slug}
+`);
+
+export const POST_QUERY = defineQuery(`
+    *[_type == "post" && defined(slug.current)]
+    [0]
+    {title, boyd, mainImage}
+`);
